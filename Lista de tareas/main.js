3d5aeeb9912c55fd5f230bfc7f7397457dd23383
DiegoSoriaArg <diegosoriaarg@gmail.com>
2023-03-02 20:52:10 -0300

    .

--- Lista de tareas/main.js
+++ Lista de tareas/main.js
@@ -1,22 +1,51 @@
-const btn = document.querySelector("[data-form-btn]");
+(() => {
 
-const crearTarea = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const tarea = document.querySelector("[data-tareas]");
-    input.value = "";
+    const btn = document.querySelector("[data-form-btn]");
 
-    //backticks
-    const contenido = `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-        </div>
+    const crearTarea = (evento) => {
+      evento.preventDefault();
+      const input = document.querySelector("[data-form-input]");
+      const value = input.value;
+      const lista = document.querySelector("[data-lista]");
+      const tarea = document.createElement("li");
+      tarea.classList.add("card");
+      input.value = "";
+
+      //backticks
+      const tareaContenido = document.createElement("div");
+      tareaContenido.appendChild(checkComplete());
+      const tituloTarea = document.createElement("span");
+      tituloTarea.classList.add("task");
+      tituloTarea.innerText = value;
+      tareaContenido.appendChild(tituloTarea);
+
+      const contenido = `
         <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    
-    tarea.innerHTML = contenido;
 
-};
+      //tarea.innerHTML = contenido;
 
-//Arrow function o funciones anonimas
-btn.addEventListener("click", crearTarea);
+      tarea.appendChild(tareaContenido);
+
+      lista.appendChild(tarea);
+    };
+
+    //Arrow function o funciones anonimas
+    btn.addEventListener("click", crearTarea);
+
+    const checkComplete = () => {
+      const i = document.createElement("i");
+      i.classList.add("far", "fa-check-square", "icon");
+      i.addEventListener("click", completeTask);
+      return i;
+    };
+
+    // Immediately invoked function expression IIFE
+    const completeTask = (event) => {
+      const elemento = event.target;
+      elemento.classList.toggle("fas");
+      elemento.classList.toggle("completeIcon");
+      elemento.classList.toggle("far");
+    };
+
+
+})();
