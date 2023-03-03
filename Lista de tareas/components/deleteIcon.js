8fe1b430c9ae9d5e7a900edd90ea874c59bf9c79
DiegoSoriaArg <diegosoriaarg@gmail.com>
2023-03-02 21:45:50 -0300

    separado por modulos

---
+++ Lista de tareas/components/deleteIcon.js
@@ -0,0 +1,13 @@
+const deleteIcon = () => {
+  const i = document.createElement("i");
+  i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+  i.addEventListener("click", deleteTarea);
+  return i;
+};
+
+const deleteTarea = (event) => {
+  const parent = event.target.parentElement;
+  parent.remove();
+};
+
+export default deleteIcon;
--- Lista de tareas/main.js
+++ Lista de tareas/main.js
@@ -1,58 +1,30 @@
-(() => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
-    const btn = document.querySelector("[data-form-btn]");
+const btn = document.querySelector("[data-form-btn]");
 
-    const crearTarea = (evento) => {
-      evento.preventDefault();
-      const input = document.querySelector("[data-form-input]");
-      const value = input.value;
-      const lista = document.querySelector("[data-lista]");
-      const tarea = document.createElement("li");
-      tarea.classList.add("card");
-      input.value = "";
+const crearTarea = (evento) => {
+  evento.preventDefault();
+  const input = document.querySelector("[data-form-input]");
+  const value = input.value;
+  const lista = document.querySelector("[data-lista]");
+  const tarea = document.createElement("li");
+  tarea.classList.add("card");
+  input.value = "";
 
-      //backticks
-      const tareaContenido = document.createElement("div");
-      tareaContenido.appendChild(checkComplete());
-      const tituloTarea = document.createElement("span");
-      tituloTarea.classList.add("task");
-      tituloTarea.innerText = value;
-      tareaContenido.appendChild(tituloTarea);
+  //backticks
+  const tareaContenido = document.createElement("div");
+  tareaContenido.appendChild(checkComplete());
+  const tituloTarea = document.createElement("span");
+  tituloTarea.classList.add("task");
+  tituloTarea.innerText = value;
+  tareaContenido.appendChild(tituloTarea);
 
-      tarea.appendChild(tareaContenido);
-      tarea.appendChild(deleteIcon());
-    
-      lista.appendChild(tarea);
-    };
+  tarea.appendChild(tareaContenido);
+  tarea.appendChild(deleteIcon());
 
-    //Arrow function o funciones anonimas
-    btn.addEventListener("click", crearTarea);
+  lista.appendChild(tarea);
+};
 
-    const checkComplete = () => {
-      const i = document.createElement("i");
-      i.classList.add("far", "fa-check-square", "icon");
-      i.addEventListener("click", completeTask);
-      return i;
-    };
-
-    // Immediately invoked function expression IIFE
-    const completeTask = (event) => {
-      const elemento = event.target;
-      elemento.classList.toggle("fas");
-      elemento.classList.toggle("completeIcon");
-      elemento.classList.toggle("far");
-    };
-
-    const deleteIcon = () => {
-        const i = document.createElement("i");
-        i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
-        i.addEventListener("click", deleteTarea);
-        return i;
-    };
-
-    const deleteTarea = (event) => {
-        const parent = event.target.parentElement;
-        parent.remove();
-    };
-
-})();
+//Arrow function o funciones anonimas
+btn.addEventListener("click", crearTarea);
